09941e6f92bece8d7d39db3d7dcfba8fcd2885b0
Maxim Liutsko <mcmaxwell07@gmail.com>
2020-01-17 15:51:30 +0200

    fixes

--- app/design/frontend/NewThemes/NewTheme/web/js/alignItems.js
+++ app/design/frontend/NewThemes/NewTheme/web/js/alignItems.js
@@ -1,24 +1,17 @@
 define(["jquery", "domReady!"], function($) {
   "use strict";
   $.widget("mage.alignitems", {
-      options: {
-        priceItem: null,
-        details: null,
-      },
     _create: function(config, elements) {
       var elements = $(this.element[0]).children();
       var self = this;
       self.changeHeights(elements);
-      console.log(this.options);
 
       $(document).on("swatch.initialized", function() {
-
         self.changeHeights(elements);
       });
     },
     changeHeights: function(elements) {
       var maxHeight = 0;
-      console.log(1);
 
       $(elements).each(function() {
         var height_block = $(this).height();
